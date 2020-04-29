94e512ad329204127f8cb1eb9bb5ec3632a643eb
mohiuddin06617 <mohiuddin06617@gmail.com>
2020-04-29 20:12:51 +0600

    Untrack files in .gitignore

---
+++ Modules/Notice/Resources/views/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('content')
+
+@endsection
+
+@push('stylesheets')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{asset('assets/modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset('assets/modules/datatables.net-select-bs4/css/select.bootstrap4.min.css')}}"/>
+@endpush
+
+@push('scripts')
+    <script src="{{asset('assets/modules/datatables/media/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('assets/modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+    <script src="{{asset('assets/modules/datatables.net-select-bs4/js/select.bootstrap4.min.js')}}"></script>
+@endpush
